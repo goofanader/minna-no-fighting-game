beda407abeb98c9db3280cc031457ed4e66f8a03
Phyllis Douglas <goofanader@gmail.com>
2015-11-06 17:21:58 -0800

    One Day, I'll Be There
    I'm still working on it...

--- public_html/avatar_creator.php
+++ public_html/avatar_creator.php
@@ -1,39 +1,74 @@
 <html>
 
 <head>
-  <title>Minna no Avatar</title>
-  <meta charset="UTF-8">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <script language="javascript" type="text/javascript" src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+  <script language="javascript" type="text/javascript" src="js/avatar_creator.js"></script>
 
+  <!-- Bootstrap -->
   <link rel="stylesheet" href="css/reset.css">
-  <link rel="stylesheet" href="css/avatar_creator.css">
+  <link href="bootstrap-3.3.5-dist/css/bootstrap.min.css" rel="stylesheet">
+  <!--<link rel="stylesheet" href="css/avatar_creator.css">-->
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+  <title>Minna no Avatar</title>
 </head>
 
 <body>
-  <div class="container">
-    <ul class="tabs">
-      <li class="tab-link current" data-tab="tab-1">Tab One</li>
-      <li class="tab-link" data-tab="tab-2">Tab Two</li>
-      <li class="tab-link" data-tab="tab-3">Tab Three</li>
-      <li class="tab-link" data-tab="tab-4">Tab Four</li>
-    </ul>
+  <!--<div class="center-layout">
+    <div class="container">
+      <ul class="tabs">
+        <li class="tab-link current" data-tab="tab-body">o-|-&lt;</li>
+        <li class="tab-link" data-tab="tab-eyes">.__.</li>
+        <li class="tab-link" data-tab="tab-mouth">:\</li>
+        <li class="tab-link" data-tab="tab-hair">&lt;o-|-&lt;</li>
+      </ul>
 
-    <div id="tab-1" class="tab-content current">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      <div id="tab-body" class="tab-content current">
+        Change body color here. Increase chest size here.
+      </div>
+      <div id="tab-eyes" class="tab-content">
+        Change eye shape here. Change eye color here.
+      </div>
+      <div id="tab-mouth" class="tab-content">
+        Change mouth shape here. Change certain areas of the mouth's colors here.
+      </div>
+      <div id="tab-hair" class="tab-content">
+        Change hair style here. Set hair color here.
+      </div>
     </div>
-    <div id="tab-2" class="tab-content">
-      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </div>
-    <div id="tab-3" class="tab-content">
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-    </div>
-    <div id="tab-4" class="tab-content">
-      Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    </div>
+  </div>
+
+  <div>-->
+
+  <!-- Nav tabs -->
+  <div>
+  <ul class="nav nav-pills nav-justified" role="tablist">
+    <li role="presentation" class="active"><a href="#body" aria-controls="body" role="tab" data-toggle="pill">o-|-&lt;</a></li>
+    <li role="presentation"><a href="#eyes" aria-controls="eyes" role="tab" data-toggle="pill">.__.</a></li>
+    <li role="presentation"><a href="#mouth" aria-controls="mouth" role="tab" data-toggle="pill">:\</a></li>
+    <li role="presentation"><a href="#hair" aria-controls="hair" role="tab" data-toggle="pill">&lt;o-|-&lt;</a></li>
+  </ul>
+
+  <!-- Tab panes -->
+  <div class="tab-content">
+    <div role="tabpanel" class="tab-pane fade in active" id="body">Change body color here. Increase chest size here.</div>
+    <div role="tabpanel" class="tab-pane fade" id="eyes">Change eye shape here. Change eye color here.</div>
+    <div role="tabpanel" class="tab-pane fade" id="mouth">Change mouth shape here. Change certain areas of the mouth's colors here.</div>
+    <div role="tabpanel" class="tab-pane fade" id="hair">Change hair style here. Set hair color here.</div>
+  </div>
 
   </div>
-  <script language="javascript" type="text/javascript" src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-  <script language="javascript" type="text/javascript" src="js/avatar_creator.js"></script>
 </body>
 
 </html>
