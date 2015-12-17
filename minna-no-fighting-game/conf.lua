8e8d814da92f534acb038a0e21b28f025cf8c09f
Phyllis Douglas <goofanader@gmail.com>
2015-12-16 19:17:40 -0800

    Adding Start of Game Files
    Will be adding more soon

---
+++ minna-no-fighting-game/conf.lua
@@ -0,0 +1,11 @@
+function love.conf(t)
+   t.identity = "Minna no Fighting Game"
+   t.version = "0.9.2"
+   t.console = true
+   t.window.title = "Minna no Fighting Game"
+   t.window.width = 800
+   t.window.height = 600
+   t.window.minwidth = 400
+   t.window.minheight = 300
+   t.window.resizable = false
+end
