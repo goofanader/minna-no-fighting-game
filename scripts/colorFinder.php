9f0c045652703daa2c43a2fca70ee9e583c76a7c
Phyllis Douglas <goofanader@gmail.com>
2015-12-30 08:04:46 -0800

    Starting Color Finder
    It’s beginning. The magic is happening. The db has been set up, I just
    need to add all the php files next.

---
+++ scripts/colorFinder.php
@@ -0,0 +1,49 @@
+<?php
+// folder is public_html/media/images/*, ignoring the ICONS folder
+
+// want to for-loop through each image in each folder
+// create a giant text document in the main area of images folder
+// ...I think?
+// for now? I guess? We'll parse it as a .tab file
+
+// email key: hash of email and then hash of secret message (TBD).
+
+// usage message
+function printUsage($isExiting) {
+  $usage = "usage: {$argv[0]} -d <directory of images and subdirectories to find colors>\n";
+
+  if ($isExiting) {
+    exit($usage);
+  } else {
+    echo $usage;
+  }
+}
+
+// check options first
+function checkOpts() {
+  $options = getopt("d:");
+
+  if (array_key_exists("d", $options)) {
+    return $options["d"];
+  } else {
+    printUsage(true);
+  }
+
+  return null;
+}
+
+// if this file is called directly, start the magic
+// help from http://stackoverflow.com/questions/4545878/how-to-know-if-php-script-is-called-via-require-once
+if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
+  $directory = checkOpts();
+
+  // check if the given thing is a valid directory
+  if (!is_dir($directory)) {
+    printUsage(true);
+  }
+
+  foreach(scandir($directory) as $file) {
+    echo $file . "\n";
+  }
+}
+ ?>
