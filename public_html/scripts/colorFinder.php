fc7a7c411e99d82e676c708c25a24adfdda24699
Phyllis Douglas <goofanader@gmail.com>
2015-12-31 08:45:25 -0800

    Colors Entered into DB
    Script is ready to be run nightly.

--- public_html/scripts/colorFinder.php
+++ public_html/scripts/colorFinder.php
@@ -7,6 +7,7 @@
 // for now? I guess? We'll parse it as a .tab file
 
 include_once("../includes/Db.php");
+$imagesInDB = array();
 
 // usage message
 function printUsage($isExiting, $programName) {
@@ -55,6 +56,8 @@ function goThroughDirectory($directory, $db) {
     return;
   }
 
+  global $imagesInDB;
+
   foreach (scandir($directory) as $file) {
     if ($file[0] != "." && $file != "ICONS") {
       $realFilePath = $directory."/".$file;
@@ -86,13 +89,74 @@ function goThroughDirectory($directory, $db) {
             $alpha = imagecolorsforindex($imageResource, $rgb);
             $alpha = $alpha["alpha"];
 
-            if ($newColor != "#000000" && $alpha < 127 && !array_key_exists($newColor, $colors)) {
+            if ($alpha < 127 && $newColor != "#000000" && !array_key_exists($newColor, $colors)) {
               $colors[$newColor] = $newColor;
             }
           }
         }
 
-        if (!empty($colors)) {
+        if (empty($colors)) {
+          $colors['#000000'] = '#000000';
+        }
+
+        $newFileName = str_replace("../", "", $realFilePath);
+
+        // create entry in db or modify existing entry
+        if (array_key_exists($newFileName, $imagesInDB)) {
+          // modify existing row.
+          // TODO: don't modify existing row if all the colors are the same!!
+          $queryString = 'UPDATE CustomizationChoices SET ';
+          $colors = array_keys($colors);
+
+          for ($i = 0; $i < count($colors); $i++) {
+            $queryString .= "`color".strval($i + 1)."` = ".$db->quote($colors[$i]);
+
+            if ($i < count($colors) - 1) {
+              $queryString .= ",";
+            }
+
+            $queryString .= " ";
+          }
+          $queryString .= "WHERE id = ".$imagesInDB[$newFileName];
+
+          $result = $db->query($queryString);
+
+          if ($result) {
+            echo "Updated row $newFileName. Query:\n";
+          } else {
+            echo $db->error()."\n";
+            echo "Could not update row $newFileName. Query:\n";
+          }
+          echo "\t$queryString\n";
+        } else {
+          // create entry in db
+          $colors = array_keys($colors);
+
+          $queryString = "INSERT INTO CustomizationChoices (filename, ";
+          $queryValues = $db->quote($newFileName).", ";
+
+          for ($i = 0; $i < count($colors); $i++) {
+            $queryString .= "`color".strval($i + 1)."`";
+            $queryValues .= $db->quote($colors[$i]);
+
+            if ($i < count($colors) - 1) {
+              $queryString .= ", ";
+              $queryValues .= ", ";
+            }
+          }
+          $queryString .= ") VALUES ($queryValues)";
+
+          $result = $db->Query($queryString);
+          if ($result){
+            echo "Adding row $newFileName. Query:\n";
+          } else {
+            echo $db->error()."\n";
+            echo "Could not add row $newFileName. Query:\n";
+          }
+          echo "\t$queryString\n";
+        }
+
+        /*if (!empty($colors)) {
           echo "File $file has these colors:\n";
           $count = 1;
           foreach ($colors as $key) {
@@ -101,7 +165,7 @@ function goThroughDirectory($directory, $db) {
           }
         } else {
           echo "$file had no colors other than black.\n";
-        }
+        }*/
         echo "-----------------------\n\n";
       }
     }
@@ -121,7 +185,18 @@ if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
 
   // get db connection
   $db = new Db();
-  //print_r($db->query("SHOW TABLES"));
+  //get the list of images already in the db
+  $rows = $db->select("SELECT id, filename FROM CustomizationChoices");
+
+  if ($rows == false) {
+    echo "{$db->error()}\n";
+    //exit("Could not successfully query the database. Please check your connection.\n");
+  }
+
+  //make a hash table of the values in the rows
+  foreach ($rows as $row) {
+    $imagesInDB[$row['filename']] = $row['id'];
+  }
 
   goThroughDirectory($directory, $db);
 } else {
