642ee9e45462db539670b9a22a0499facfe650c0
Phyllis Douglas <goofanader@gmail.com>
2015-12-31 13:33:12 -0800

    Final Stuff for Color Finder
    Sends messages to stderr when there's errors. Doesn't always update rows
    in the db. It's all prepared to be called by a cronjob every night now.

--- public_html/scripts/colorFinder.php
+++ public_html/scripts/colorFinder.php
@@ -7,7 +7,16 @@
 // for now? I guess? We'll parse it as a .tab file
 
 include_once("../includes/Db.php");
+define("MAX_COLORS", 5);
 $imagesInDB = array();
+
+function printErrorMessage($msg, $STDERR) {
+  echo $msg;
+
+  if (isset($STDERR)) {
+    fwrite($STDERR, $msg);
+  }
+}
 
 // usage message
 function printUsage($isExiting, $programName) {
@@ -49,6 +58,20 @@ function getHexColorString($image, $x, $y) {
   return "#".$r.$g.$b;
 }
 
+function hasDifferentColors($colors1, $colors2) {
+  if (count($colors1) != count($colors2)) {
+    return true;
+  }
+
+  for ($i = 0; $i < count($colors1); $i++) {
+    if ($colors1[$i] != $colors2[$i]) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 // recursive function to go through directories
 function goThroughDirectory($directory, $db) {
   // don't handle the directory file if it's the ICONS folder
@@ -56,7 +79,7 @@ function goThroughDirectory($directory, $db) {
     return;
   }
 
-  global $imagesInDB;
+  global $imagesInDB, $STDERR;
 
   foreach (scandir($directory) as $file) {
     if ($file[0] != "." && $file != "ICONS") {
@@ -71,7 +94,7 @@ function goThroughDirectory($directory, $db) {
         // should really check to make sure it's of PNG type
         $imageResource = imagecreatefrompng($realFilePath);
         if (!$imageResource) {
-          echo "Could not process $file.\n";
+          printErrorMessage("Could not process $file.\n", $STDERR);
           continue;
         }
 
@@ -104,9 +127,21 @@ function goThroughDirectory($directory, $db) {
         // create entry in db or modify existing entry
         if (array_key_exists($newFileName, $imagesInDB)) {
           // modify existing row.
-          // TODO: don't modify existing row if all the colors are the same!!
+
           $queryString = 'UPDATE CustomizationChoices SET ';
           $colors = array_keys($colors);
+          if (count($colors) > MAX_COLORS) {
+            // TODO: add a new column
+            printErrorMessage("There are ".strval(count($colors))." colors in $newFileName. Please limit the number of colors!\n", $STDERR);
+            continue;
+          }
+
+          // don't modify existing row if all the colors are the same!!
+          if (!hasDifferentColors($colors, $imagesInDB[$newFileName]['colors'])) {
+            echo "$newFileName is the same.\n";
+            echo "-----------------------\n";
+            continue;
+          }
 
           for ($i = 0; $i < count($colors); $i++) {
             $queryString .= "`color".strval($i + 1)."` = ".$db->quote($colors[$i]);
@@ -117,20 +152,25 @@ function goThroughDirectory($directory, $db) {
 
             $queryString .= " ";
           }
-          $queryString .= "WHERE id = ".$imagesInDB[$newFileName];
+          $queryString .= "WHERE id = ".$imagesInDB[$newFileName]["id"];
 
           $result = $db->query($queryString);
 
           if ($result) {
             echo "Updated row $newFileName. Query:\n";
+            echo "\t$queryString\n";
+            echo "-----------------------\n";
           } else {
-            echo $db->error()."\n";
-            echo "Could not update row $newFileName. Query:\n";
+            printErrorMessage($db->error()."\nCould not update row $newFileName. Query:\n\t$queryString\n-----------------------\n", $STDERR);
           }
-          echo "\t$queryString\n";
         } else {
           // create entry in db
           $colors = array_keys($colors);
+          if (count($colors) > MAX_COLORS) {
+            // TODO: add a new column
+            printErrorMessage("There are ".strval(count($colors))." colors in $newFileName. Please limit the number of colors!\n", $STDERR);
+            continue;
+          }
 
           $queryString = "INSERT INTO CustomizationChoices (filename, ";
           $queryValues = $db->quote($newFileName).", ";
@@ -149,24 +189,12 @@ function goThroughDirectory($directory, $db) {
           $result = $db->Query($queryString);
           if ($result){
             echo "Adding row $newFileName. Query:\n";
+            echo "\t$queryString\n";
+            echo "-----------------------\n";
           } else {
-            echo $db->error()."\n";
-            echo "Could not add row $newFileName. Query:\n";
+            printErrorMessage($db->error()."\nCould not add row $newFileName. Query:\n\t$queryString\n-----------------------\n", $STDERR);
           }
-          echo "\t$queryString\n";
         }
-
-        /*if (!empty($colors)) {
-          echo "File $file has these colors:\n";
-          $count = 1;
-          foreach ($colors as $key) {
-            echo "\t$count. $key\n";
-            $count++;
-          }
-        } else {
-          echo "$file had no colors other than black.\n";
-        }*/
-        echo "-----------------------\n\n";
       }
     }
   }
@@ -177,25 +205,38 @@ function goThroughDirectory($directory, $db) {
 if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
   $directory = checkOpts($argv);
 
+  // set up the stderr stream
+  $STDERR = fopen('php://stderr', 'w+');
+
   // check if the given thing is a valid directory
   if (!is_dir($directory)) {
-    echo "Not a valid directory!\n";
+    printErrorMessage("Not a valid directory!\n", $STDERR);
     printUsage(true, $argv[0]);
   }
 
   // get db connection
   $db = new Db();
   //get the list of images already in the db
-  $rows = $db->select("SELECT id, filename FROM CustomizationChoices");
+  $rows = $db->select("SELECT * FROM CustomizationChoices");
 
   if ($rows == false) {
-    echo "{$db->error()}\n";
-    //exit("Could not successfully query the database. Please check your connection.\n");
+    printErrorMessage("Could not get the list of images already in the db! {$db->error()}\n", $STDERR);
   }
 
   //make a hash table of the values in the rows
   foreach ($rows as $row) {
-    $imagesInDB[$row['filename']] = $row['id'];
+    $imagesInDB[$row['filename']] = array(
+      "id" => $row['id'],
+      "colors" => array()
+    );
+
+    for ($i = 1; $i < MAX_COLORS + 1; $i++) {
+      if (isset($row["color$i"])) {
+        $imagesInDB[$row['filename']]["colors"][] = $row["color$i"];
+      } else {
+        break;
+      }
+    }
   }
 
   goThroughDirectory($directory, $db);
